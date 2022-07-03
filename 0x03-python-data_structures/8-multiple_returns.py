b322fe90db3e4d48552ee2480fb9a73ef94b0368
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2022-07-03 10:16:17 +0000

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return(tup)
+    length, first_char = len(sentence), None
+    if(length > 0):
+      first_char = sentence[0]
+    result  = length, first_char
+    return result
